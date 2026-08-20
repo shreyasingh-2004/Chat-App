c0e730884d1f91d95c80506b99f10b9c97288b07
Shreya Singh <shreya031204@gmail.com>
2026-08-20 19:03:06 +0530

    Clean up: remove 26 duplicate/dead files, fix .gitignore structure

--- server/check-model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-console.log('Mongoose models:', Object.keys(mongoose.models));
-
-// Check if User model is already defined
-if (mongoose.models.User) {
-  console.log('⚠️ User model already exists! This can cause issues');
-  console.log('Model schema paths:', Object.keys(mongoose.models.User.schema.paths));
-}
